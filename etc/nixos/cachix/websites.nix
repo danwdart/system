d541fb2b1f64c27b615b38a0dd9c27028c9984d2
Dan Dart <git@dandart.co.uk>
2021-01-02 04:41:13 +0000

    cachix websites

---
+++ etc/nixos/cachix/websites.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://websites.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "websites.cachix.org-1:YMPYgEeWohlGq/0wDvWLVSRoNcBS1jIOmku6Djv7zcM="
+    ];
+  };
+}
+    
