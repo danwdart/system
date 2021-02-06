9d0f1627069b86ab859883b93bf81acb0a486e26
Dan Dart <git@dandart.co.uk>
2021-02-06 01:23:53 +0000

    add reflex-platform caches

--- altair/configuration.nix
+++ altair/configuration.nix
@@ -13,6 +13,9 @@ in {
       ./cachix.nix
     ];
 
+  nix.binaryCaches = [ "https://cache.nixos.org/" "https://nixcache.reflex-frp.org" ];
+  nix.binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+
   nix.trustedUsers = [ "root" "dwd" ];
 
   nix.gc.automatic = true;
