a1c6aeeceaa772d49f80f51a3804420f8c0c9ee0
Dan Dart <git@dandart.co.uk>
2021-02-10 23:51:18 +0000

    update channels afterwards

--- common/switch.sh
+++ common/switch.sh
@@ -4,3 +4,5 @@ sudo nix-channel --update
 sudo nixos-rebuild switch -I nixos-config=$PWD/configuration.nix
 sudo nix-collect-garbage -d
 sudo nix-store --optimise
+nix-channel --update
+sudo nix-channel --update
