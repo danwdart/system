e6cee01b24572618b24711b7e2986771242f4028
Dan Dart <git@dandart.co.uk>
2021-05-14 18:15:49 +0100

    remove old wine stuff

--- common/categories/emu.nix
+++ common/categories/emu.nix
@@ -5,9 +5,9 @@ in with pkgs; [
     dosbox
     mtools
     protontricks
-    unstable.winePackages.fonts
-    unstable.winePackages.staging
-    unstable.winetricks
+    #unstable.winePackages.fonts
+    #unstable.winePackages.staging
+    # unstable.winetricks # depend on correct version
     unstable.wineWowPackages.fonts
     unstable.wineWowPackages.staging
 ]
