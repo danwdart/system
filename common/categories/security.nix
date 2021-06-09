cf26034af1f702f8ef89fa4698863b5eee5c7dbf
Dan Dart <git@dandart.co.uk>
2021-06-09 21:31:37 +0100

    Update security apps

--- common/categories/security.nix
+++ common/categories/security.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 with pkgs; [
+    aircrack-ng
     chkrootkit
+    john
+    thc-hydra
     lynis
     metasploit
     nmap
-    ossec
-    pam_usb
-    # rkhunter
-    # tripwire
+    openssl # the CLI client
+    # ossec - not yet integrated into systemd
+    # pam_usb - already an option
+    pyrit
+    # rkhunter - not yet available
+    # tripwire - not yet available - unsure if any point?
 ]
