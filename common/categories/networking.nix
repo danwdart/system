1cc372e71dbe988bf2b532d9ed5b11cb54938b7b
Dan Dart <git@dandart.co.uk>
2021-07-20 03:02:10 +0100

    nextcloud and remove torbrowser until it works

--- common/categories/networking.nix
+++ common/categories/networking.nix
@@ -2,12 +2,12 @@
 with pkgs; [
     bind # host, nslookup etc
     doctl # run doctl auth init before use
-    ipfs
     irssi
     mailutils
+    nextcloud-client
     putty # no desktop icon
     rdesktop # no gui without config
     scrcpy
     tightvnc # no gui without config
-    torbrowser
+    # tor-browser-bundle-bin # 404?
 ]
