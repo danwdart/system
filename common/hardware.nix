f50e864397468ff01100b195f62de987dd1ab28a
Dan Dart <git@dandart.co.uk>
2021-08-02 22:37:43 +0100

    microcode and restructure and clean hardware.nix

--- common/hardware.nix
+++ common/hardware.nix
@@ -1,39 +1,19 @@
 { pkgs, unstable, ... }:
 {
-  pulseaudio = {
-    enable = false;
-    /*
-    extraModules = with pkgs; [
-      pulseaudio-modules-bt
-    ];
-    package = pkgs.pulseaudioFull;
-    tcp = {
-      enable = true;
-      anonymousClients = {
-        allowedIpRanges = [
-          "127.0.0.1"
-          "192.168.1.0/24"
-        ];
-      };
-    };
-    support32Bit = true;
-    zeroconf = {
-      discovery = {
-        enable = true;
-      };
-      publish = {
-        enable = true;
-      };
-    };
-    */
+  cpu.intel.updateMicrocode = true;
+
+  pulseaudio.enable = false;
+
+  opengl = {
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libva unstable.pkgsi686Linux.amdvlk ];
+    extraPackages = with pkgs; [ amdvlk rocm-opencl-icd rocm-opencl-runtime ];
   };
 
-  opengl.driSupport = true;
-  opengl.driSupport32Bit = true;
-  opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva unstable.pkgsi686Linux.amdvlk ];
-  opengl.extraPackages = with pkgs; [ amdvlk rocm-opencl-icd rocm-opencl-runtime ];
-
-  bluetooth.enable = true;
-  bluetooth.powerOnBoot = true;
-  bluetooth.package = pkgs.bluezFull;
+  bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    package = pkgs.bluezFull;
+  };
 }
