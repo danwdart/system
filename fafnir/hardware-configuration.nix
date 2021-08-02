c4ce0dd827b9dc58955387a41ada4d8cd63e3f34
Dan Dart <git@dandart.co.uk>
2021-08-02 21:12:08 +0100

    restructure filesystems

--- fafnir/hardware-configuration.nix
+++ fafnir/hardware-configuration.nix
@@ -2,7 +2,32 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
-
+let
+  device = uuid: "/dev/disk/by-uuid/${uuid}";
+  btrfsDevice = device "9b7beb2f-1f4d-49e7-990d-1dab4f611fa7";
+  bootDevice = device "860D-1A99";
+  swapDevice = device "55f0c8d9-20d2-4af1-a586-25e8a97847e4";
+  btrfsMount = subvol: {
+    device = btrfsDevice;
+    fsType = "btrfs";
+    options = [
+      "subvol=${subvol}"
+      "noatime"
+    ];
+    neededForBoot = true;
+  };
+  bootMount = {
+    device = bootDevice;
+    fsType = "vfat";
+  };
+  rootMount = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [
+      "size=2G"
+    ];
+  };
+in
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
@@ -13,92 +38,32 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   
-  fileSystems."/" =
-    {
-      device = "tmpfs";
-      fsType = "tmpfs";
-      options = [
-        "size=2G"
-      ];
-    };
+  fileSystems = {
+    "/" = rootMount;
+    # not in persistence because we don't want to back it up
+    "/nix" = btrfsMount "/nix";
+    "/boot" = bootMount;
+    # TODO move to persistence
+    "/var/lib" = btrfsMount "/var/lib";
+    "/persist" = btrfsMount "/persist";
+  };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/9b7beb2f-1f4d-49e7-990d-1dab4f611fa7";
-      fsType = "btrfs";
-      options = [
-        "subvol=/nix"
-        "noatime"
-      ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/860D-1A99";
-      fsType = "vfat";
-    };
-
-  fileSystems."/etc/ssh" =
-    {
-      device = "/dev/disk/by-uuid/9b7beb2f-1f4d-49e7-990d-1dab4f611fa7";
-      fsType = "btrfs";
-      options = [
-        "subvol=/etc/ssh"
-        "noatime"
-      ];
-    };
-
-  fileSystems."/etc/NetworkManager" =
-    {
-      device = "/dev/disk/by-uuid/9b7beb2f-1f4d-49e7-990d-1dab4f611fa7";
-      fsType = "btrfs";
-      options = [
-        "subvol=/etc/NetworkManager"
-        "noatime"
-      ];
-    };
-  
-  fileSystems."/var/lib" =
-    {
-      device = "/dev/disk/by-uuid/9b7beb2f-1f4d-49e7-990d-1dab4f611fa7";
-      fsType = "btrfs";
-      options = [
-        "subvol=/var/lib"
-        "noatime"
-      ];
-    };
-
-  fileSystems."/persist" =
-    {
-      device = "/dev/disk/by-uuid/9b7beb2f-1f4d-49e7-990d-1dab4f611fa7";
-      fsType = "btrfs";
-      options = [
-        "subvol=/persist"
-        "noatime"
-      ];
-    };
-
-  #environment.persistence."/persist-no-backup" = {
-  #  directories = [
-  #    "/nix"
-  #  ];
-  #  files = [
-  #  ];
-  #};
-
-  #environment.persistence."/persist" = {
-  #  directories = [
-  #    "/etc/ssh"
-  #    "/etc/NetworkManager"
-  #  ];
-  #  files = [
-  #  ];
-  #};
-  
   swapDevices = [
     {
-      device = "/dev/disk/by-uuid/55f0c8d9-20d2-4af1-a586-25e8a97847e4";
+      device = swapDevice;
     }
   ];
 
+  environment.persistence."/persist" = {
+    directories = [
+      "/etc/ssh"
+      "/etc/NetworkManager"
+      # "/var/lib"
+    ];
+    files = [
+    ];
+  };
+  
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
 }
