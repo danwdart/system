58cf272d9f423935677517cbb9997a88e41245db
Dan Dart <git@dandart.co.uk>
2021-09-20 17:14:49 +0100

    remove virt stuff as it's in exprs

--- common/categories/virt.nix
+++ common/categories/virt.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 with pkgs; [
     # libguestfs-with-appliance # keeps compiling
-    OVMF
-    OVMF-CSM
-    OVMF-secureBoot
-    qemu
-    virglrenderer
+    # OVMF
+    # OVMF-CSM
+    # OVMF-secureBoot
+    # qemu
+    # virglrenderer
     # virtinst
-    virt-manager
+    # virt-manager
     # virt-manager-qt
-    virt-viewer # desktop icon: "Remote Viewer"
-    win-qemu
+    # virt-viewer # desktop icon: "Remote Viewer"
+    # #win-qemu
 ]
