932073f3fa48a2192ef8651712c2d0208065e6d5
Dan Dart <git@dandart.co.uk>
2021-10-22 19:26:31 +0100

    remove amd graphics packages

--- common/hardware.nix
+++ common/hardware.nix
@@ -7,8 +7,8 @@
   opengl = {
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages32 = with pkgs.pkgsi686Linux; [ libva unstable.pkgsi686Linux.amdvlk ];
-    extraPackages = with pkgs; [ amdvlk rocm-opencl-icd rocm-opencl-runtime ];
+    #extraPackages32 = with pkgs.pkgsi686Linux; [ libva unstable.pkgsi686Linux.amdvlk ];
+    #extraPackages = with pkgs; [ amdvlk rocm-opencl-icd rocm-opencl-runtime ];
   };
 
   bluetooth = {
