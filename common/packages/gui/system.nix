3d2867b5a10a1d670c60a8d80b9d286907365f13
Dan Dart <git@dandart.co.uk>
2021-10-22 19:24:55 +0100

    remove unused system gui packages

--- common/packages/gui/system.nix
+++ common/packages/gui/system.nix
@@ -1,12 +1,12 @@
 { pkgs, unstable, ... }:
 with pkgs; [
-    etcher
+    #etcher
     gparted
-    unstable.rpi-imager
-    unetbootin
-    winusb
-    woeusb
-    xcruiser # no desktop icon
-    xorg.xev
+    #unstable.rpi-imager
+    #unetbootin
+    #winusb
+    #woeusb
+    #xcruiser # no desktop icon
+    #xorg.xev
     xorg.xf86videointel
 ]
