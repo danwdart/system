cc48aa89d4f660f6c90953687486048101c1274a
Dan Dart <git@dandart.co.uk>
2021-10-22 19:25:24 +0100

    pass master to packages

--- common/packages/packages.nix
+++ common/packages/packages.nix
@@ -1,6 +1,6 @@
-{ pkgs, unstable, ... }:
+{ pkgs, unstable, master, ... }:
 # Any defaults/must-haves go here
 []
 # Any categorical apps go here
-    ++ import ./console/packages.nix {pkgs = pkgs; unstable = unstable;}
-    ++ import ./gui/packages.nix {pkgs = pkgs; unstable = unstable;}
+    ++ import ./console/packages.nix {pkgs = pkgs; unstable = unstable; master = master;}
+    ++ import ./gui/packages.nix {pkgs = pkgs; unstable = unstable; master = master;}
