818201d47084573b58b92fc58984b743fec6e961
Dan Dart <git@dandart.co.uk>
2021-11-18 20:00:49 +0000

    Add pcsx2 and more wii stuff

--- common/packages/gui/emu.nix
+++ common/packages/gui/emu.nix
@@ -1,10 +1,15 @@
 { pkgs, master, ... }:
 with pkgs; [
     dosbox
+    pcsx2 # keeps recompiling
+    # pcsxr # depends on insecure ffmpeg
     #protontricks
     #unstable.winePackages.fonts
-    master.winePackages.staging
+    # master.winePackages.staging
     # unstable.winetricks # depend on correct version
     #unstable.wineWowPackages.fonts
     master.wineWowPackages.staging
+    wiiload
+    wiimms-iso-tools
+    wiiuse
 ]
