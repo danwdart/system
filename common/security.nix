45d1f84370066fce1c1d4ba60a05c6a6ced05f66
Dan Dart <git@dandart.co.uk>
2022-01-22 11:32:45 +0000

    Add private certs into store

--- common/security.nix
+++ common/security.nix
@@ -1,8 +1,15 @@
-{...}:
+{pkgs, ...}:
 {
   acme.email = "acme@dandart.co.uk";
   acme.acceptTerms = true;
 
   # pam.usb.enable = true;
   # TODO pam phone fingerprint?
+
+  pki.certificateFiles = [
+    "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    # ../common/private/ca-bundle.crt
+    /persist/home/dwd/code/mine/nix/system/common/private/rootCA.pem
+    /persist/home/dwd/code/mine/nix/system/common/private/local-cert.pem
+  ];
 }
