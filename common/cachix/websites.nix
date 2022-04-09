0ec14f260b472ee0d98923de64fd47002e096e0f
Dan Dart <git@dandart.co.uk>
2022-04-09 14:14:57 +0100

    add static haskell nix to cachix

--- common/cachix/websites.nix
+++ common/cachix/websites.nix
@@ -2,9 +2,11 @@
   nix = {
     binaryCaches = [
       "https://websites.cachix.org"
+      "https://static-haskell-nix.cachix.org"
     ];
     binaryCachePublicKeys = [
       "websites.cachix.org-1:YMPYgEeWohlGq/0wDvWLVSRoNcBS1jIOmku6Djv7zcM="
+      "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
     ];
   };
 }
