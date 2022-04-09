5872f36464c3fa9088a878e8b7e2c8d76f32c1c2
Dan Dart <git@dandart.co.uk>
2022-04-09 14:15:19 +0100

    stop adding unstable

--- common/scripts/switch.sh
+++ common/scripts/switch.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # This seems not to be being persisted.
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
 sudo nice -19 nixos-rebuild switch --upgrade-all -I nixos-config=$PWD/configuration.nix
