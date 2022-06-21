d5923c7cec1e1589c8d140acf56dcb5622860417
Dan Dart <git@dandart.co.uk>
2022-06-21 01:48:28 +0100

    ham tools

--- common/packages/gui/hamradio.nix
+++ common/packages/gui/hamradio.nix
@@ -2,14 +2,33 @@ pkgs:
 with pkgs; [
     chirp
     cqrlog
+    cubicsdr
+    dabtools
     # direwolf # compilation issues?
     fldigi
     flrig
     freedv
+    # gnss-sdr
+    gnuradio
     gpredict
+    gqrx
+    # guglielmo
+    hackrf
+    inspectrum
+    kalibrate-hackrf
+    nrsc5
     qsstv
+    quisk
     tqsl
+    # rtlsdr
+    sdrangel
+    soapyaudio
+    soapyhackrf
+    soapyremote
+    soapysdrplay
+    soapysdr-with-plugins
     soundmodem # no desktop icon, needs config
+    welle-io
     wsjtx
     xlog
 ]
