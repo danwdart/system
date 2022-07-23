f0f929df4d8d2213f65c3afb6a0b1bf3f7410768
Dan Dart <git@dandart.co.uk>
2022-07-23 06:19:55 +0100

    don't verify on all upgrades

--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HERE=$(dirname $0)
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
-sudo nix-store --verify --check-contents --repair
+# sudo nix-store --verify --check-contents --repair
 $HERE/update.sh
 $HERE/switch.sh
 $HERE/cleanup.sh
