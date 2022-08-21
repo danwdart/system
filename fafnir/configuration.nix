b7672170f27e6f9a4664b3260b152923603c29be
Dan Dart <git@dandart.co.uk>
2022-08-21 15:51:14 +0100

    add old state version to faf

--- fafnir/configuration.nix
+++ fafnir/configuration.nix
@@ -19,4 +19,12 @@ in {
   networking.hostName = hostName;
 
   services.openssh.banner = "Connection established to ${hostName}. Unauthorised connections are logged.\n";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
