6d3719fe56a68d0b728b1f482fc01992f51de760
Dan Dart <git@dandart.co.uk>
2022-08-21 15:42:43 +0100

    add xsane gimp

--- common/nixpkgs.nix
+++ common/nixpkgs.nix
@@ -6,5 +6,9 @@
       # ???
       "python2.7-pyjwt-1.7.1"
     ];
+    packageOverrides = pkgs: {
+      xsaneGimp = pkgs.xsane.override { gimpSupport = true; };
+      # ln -s /run/current-system/sw/bin/xsane ~/.config/GIMP/2.10/plug-ins/xsane
+    };
   };
 }
