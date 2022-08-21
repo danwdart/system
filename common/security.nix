e441d45f8356069b9ac9a8624857e8b55923a930
Dan Dart <git@dandart.co.uk>
2022-08-21 15:44:09 +0100

    enable rtkit and remove /persist for now

--- common/security.nix
+++ common/security.nix
@@ -6,10 +6,12 @@
   # pam.usb.enable = true;
   # TODO pam phone fingerprint?
 
+  rtkit.enable = true;
+
   pki.certificateFiles = [
     "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
     # ../common/private/ca-bundle.crt
-    /persist/home/dwd/code/mine/nix/system/common/private/rootCA.pem
-    /persist/home/dwd/code/mine/nix/system/common/private/local-cert.pem
+    /home/dwd/code/mine/nix/system/common/private/rootCA.pem
+    /home/dwd/code/mine/nix/system/common/private/local-cert.pem
   ];
 }
