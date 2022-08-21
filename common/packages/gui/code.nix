cfc7e713c424dd0d290cd0f5360a9426be0eb962
Dan Dart <git@dandart.co.uk>
2022-08-21 15:49:07 +0100

    add new circuit emulators under code

--- common/packages/gui/code.nix
+++ common/packages/gui/code.nix
@@ -3,7 +3,30 @@ with pkgs; [
     cool-retro-term
     androidStudioPackages.canary
     android-tools
+    circup
+    diylc
+    gnucap
+    horizon-eda
     kate
+    librepcb
+    logisim
+    logisim-evolution
+    ngspice
+    pcb
+    python310Packages.cirq-web
+    python310Packages.cirq-rigetti
+    python310Packages.cirq-pasqal
+    python310Packages.cirq-ionq
+    python310Packages.cirq-google
+    python310Packages.cirq-core
+    python310Packages.cirq-aqt
+    python310Packages.cirq
+    qucs
+    stlink
+    tkgate
     vscode # insiders?
     x11docker
+    xcircuit
+    xyce
+    xyce-parallel
 ]
