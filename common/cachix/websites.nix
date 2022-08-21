0eeafc2938f59ac501cbfb60ed3c99d172daf394
Dan Dart <git@dandart.co.uk>
2022-08-21 15:46:13 +0100

    update cachix mapkeys

--- common/cachix/websites.nix
+++ common/cachix/websites.nix
@@ -1,12 +1,14 @@
 {
   nix = {
-    binaryCaches = [
-      "https://websites.cachix.org"
-      "https://static-haskell-nix.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "websites.cachix.org-1:YMPYgEeWohlGq/0wDvWLVSRoNcBS1jIOmku6Djv7zcM="
-      "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
-    ];
+    settings = {
+      substituters = [
+        "https://websites.cachix.org"
+        "https://static-haskell-nix.cachix.org"
+      ];
+      trusted-public-keys = [
+        "websites.cachix.org-1:YMPYgEeWohlGq/0wDvWLVSRoNcBS1jIOmku6Djv7zcM="
+        "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
+        ];
+    };
   };
 }
