7af7ef04c78648cf7d28df2d13fab00b469fc583
Dan Dart <git@dandart.co.uk>
2022-11-27 01:03:37 +0000

    Disable reboot for updates

--- common/system.nix
+++ common/system.nix
@@ -2,7 +2,7 @@
 {
     autoUpgrade = {
       enable = true;
-      allowReboot = true;
+      allowReboot = false; # okay this is getting annoying, disabled until NixOS supports kexec/ksplice/etc
       #rebootWindow = {
       #    lower = "19:00";
       #    upper = "07:00";
