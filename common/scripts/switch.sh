c8763080fdd02523c6feb18c7f85409b6e5ca66d
Dan Dart <git@dandart.co.uk>
2022-12-08 18:36:52 +0000

    scripts: accept $@

--- common/scripts/switch.sh
+++ common/scripts/switch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # This seems not to be being persisted.
-sudo nice -19 nixos-rebuild switch --upgrade-all -I nixos-config=$PWD/configuration.nix
+sudo nice -19 nixos-rebuild switch --upgrade-all -I nixos-config=$PWD/configuration.nix $@
--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -3,5 +3,5 @@ HERE=$(dirname $0)
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
 # sudo nix-store --verify --check-contents --repair
 $HERE/update.sh && \
-$HERE/switch.sh && \
+$HERE/switch.sh $@ && \
 $HERE/cleanup.sh
