f1b5a178698e9ede254cae24843bfb8d07fb8691
Dan Dart <git@dandart.co.uk>
2023-02-22 20:08:02 +0000

    re-enable reboots out of hours and update hourly

--- common/system.nix
+++ common/system.nix
@@ -2,11 +2,13 @@
 {
     autoUpgrade = {
       enable = true;
-      allowReboot = false; # okay this is getting annoying, disabled until NixOS supports kexec/ksplice/etc
-      #rebootWindow = {
-      #    lower = "19:00";
-      #    upper = "07:00";
-      #};
+      # persistent?                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                = true;
+      dates = "hourly";
+      allowReboot = true; # okay this is getting annoying, disabled until NixOS supports kexec/ksplice/etc
+      rebootWindow = {
+          lower = "19:00";
+          upper = "07:00";
+      };
       flags = [
           "-I"
           "nixos-config=/home/dwd/code/mine/nix/system/${hostName}/configuration.nix"
