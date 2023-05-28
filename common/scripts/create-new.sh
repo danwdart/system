cadbcc01bc844d51591f9c2909fcc5afe0ba8f2e
Dan Dart <git@dandart.co.uk>
2023-05-28 01:04:51 +0100

    remove the create new thing for the tls certs

--- common/scripts/create-new.sh
+++ common/scripts/create-new.sh
@@ -15,7 +15,7 @@ sed -i "s/sinistra/$1/g" $1/configuration.nix
 echo Creating private directories...
 mkdir -p common/private/{tt-rss,nextcloud,msf,users/{raven,dwd,root}}
 echo Creating private files...
-touch common/private/{ca-bundle.crt,rootCA.pem,local-cert.pem,sasl_passwd,users/{root,raven,dwd}/hashed_password,tt-rss/{dbpass,email_from_address,email_login,email_password,email_server},nextcloud/{dbpass,adminpass},msf/dbpass}
+touch common/private/{sasl_passwd,users/{root,raven,dwd}/hashed_password,tt-rss/{dbpass,email_from_address,email_login,email_password,email_server},nextcloud/{dbpass,adminpass},msf/dbpass}
 echo TODO: put junk in the files! Accept emails and hostnames as params etc.
 echo TODO the picture file of me
 echo TODO passwords for users
