3180384556c7a0effb1938c67a31907de88161bd
Dan Dart <d.dart@equalsmoney.com>
2023-07-28 13:03:58 +0100

    that should help some

--- zhang/configuration.nix
+++ zhang/configuration.nix
@@ -1,20 +1,15 @@
 
 { pkgs, ... }:
-let nixpkgs = import <nixos> {
-    config = {
-        allowUnfree = true;
-    };
-};
-in
 {
     imports = [
         ./hardware-configuration.nix    
     ];
+    nixpkgs.config.allowUnfree = true;
     boot.tmp.cleanOnBoot = true;
     zramSwap.enable = true;
     networking.hostName = "zhang";
     networking.domain = "jolharg.com";
-    environment.systemPackages = import ../common/packages/console/packages.nix nixpkgs;
+    environment.systemPackages = import ../common/packages/console/packages.nix pkgs;
     services.openssh.enable = true;
     users = import ../common/users.nix { privateDir = "/home/dwd/code/mine/nix/system/zhang/private"; }; 
     system.stateVersion = "23.05";
