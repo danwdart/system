8fa328d49dddc3e8c52fde5b84d4a8ab5c220416
Dan Dart <git@dandart.co.uk>
2023-07-28 13:16:52 +0100

    unstable by default

--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 HERE=$(dirname $0)
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 # sudo nix-store --verify --check-contents --repair
 $HERE/update.sh && \
 $HERE/switch.sh $@ && \
