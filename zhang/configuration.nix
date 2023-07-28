11ba4698deff56fab581f63886ae8de23b94f490
Dan Dart <d.dart@equalsmoney.com>
2023-07-28 13:36:12 +0100

    push nix-ld to zhang and remove openvscode-server

--- zhang/configuration.nix
+++ zhang/configuration.nix
@@ -4,19 +4,28 @@
     imports = [
         ./hardware-configuration.nix    
     ];
-    nixpkgs.config = {
-        allowUnfree = true;
-        permittedInsecurePackages = [
-            "nodejs-16.20.1"
-        ];
-    };
+    nixpkgs.config.allowUnfree = true;
     boot.tmp.cleanOnBoot = true;
     zramSwap.enable = true;
     networking.hostName = "zhang";
     networking.domain = "jolharg.com";
     environment.systemPackages = import ../common/packages/console/packages.nix pkgs;
     services.openssh.enable = true;
-    services.openvscode-server.enable = true;
+    programs.nix-ld = {
+        enable = true; # breaks ls in nix shells!?
+        # Sets up all the libraries to load
+        libraries = with pkgs; [
+            stdenv.cc.cc
+            zlib
+            fuse3
+            icu
+            zlib
+            nss
+            openssl
+            curl
+            expat
+        ];
+    };
     users = import ../common/users.nix { privateDir = "/home/dwd/code/mine/nix/system/zhang/private"; }; 
     system.stateVersion = "23.05";
 }
