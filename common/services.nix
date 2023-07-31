e60bf77542a752e775133e56337fd0ba5348133a
Dan Dart <git@dandart.co.uk>
2023-07-31 21:13:11 +0100

    duh 96

--- common/services.nix
+++ common/services.nix
@@ -245,7 +245,7 @@ in {
   #    haskellPackages.xmonad-contrib
   #    haskellPackages.monad-logger
   #  ];
-  #  haskellPackages = pkgs.haskell.packages.ghc92;
+  #  haskellPackages = pkgs.haskell.packages.ghc96;
   #};
 
   # Enable touchpad support (enabled default in most desktopManager).
