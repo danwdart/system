d0b778564f7848409e27eaf5ba035c81c764d195
Dan Dart <git@dandart.co.uk>
2023-11-18 04:20:38 +0000

    temporarily disable suddenly broken postgrest

--- common/packages/console/networking.nix
+++ common/packages/console/networking.nix
@@ -6,7 +6,7 @@ with pkgs; [
     # docker-proxy
     doctl # run doctl auth init before use
     hans
-    (haskell.packages.ghc94.override { overrides = self: super: rec { fuzzyset = haskell.lib.doJailbreak (haskell.lib.markUnbroken super.fuzzyset); configurator-pg = haskell.lib.doJailbreak (haskell.lib.markUnbroken super.configurator-pg); hasql-pool = haskell.lib.dontCheck (self.callHackage "hasql-pool" "0.10" {}); postgrest = haskell.lib.dontCheck ( haskell.lib.doJailbreak (self.callCabal2nix "postgrest" (builtins.fetchTarball "https://github.com/PostgREST/postgrest/archive/refs/tags/v11.2.1.tar.gz") {})); }; }).postgrest
+    # (haskell.packages.ghc94.override { overrides = self: super: rec { fuzzyset = haskell.lib.doJailbreak (haskell.lib.markUnbroken super.fuzzyset); configurator-pg = haskell.lib.doJailbreak (haskell.lib.markUnbroken super.configurator-pg); hasql-pool = haskell.lib.dontCheck (self.callHackage "hasql-pool" "0.10" {}); postgrest = haskell.lib.dontCheck ( haskell.lib.doJailbreak (self.callCabal2nix "postgrest" (builtins.fetchTarball "https://github.com/PostgREST/postgrest/archive/refs/tags/v11.2.1.tar.gz") {})); }; }).postgrest
     iodine
     irssi
     mailutils
