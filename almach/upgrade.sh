a7b1dc8c4f742c7705a2543041ae7edb1faf6cb7
Dan Dart <d.dart@equalsmoney.com>
2024-01-05 10:14:49 +0000

    include apple software upgrade

--- almach/upgrade.sh
+++ almach/upgrade.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 trap pwd ERR
 
+sudo softwareupdate -aiR
 nix-channel --update
 darwin-rebuild -I darwin-config=$PWD/darwin-configuration.nix switch --show-trace
 nix-collect-garbage -d
+brew update
 brew upgrade
