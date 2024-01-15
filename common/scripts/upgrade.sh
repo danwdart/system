01c158f41644f80bed2b7cb838d2b871a6ec4ffd
Dan Dart <git@dandart.co.uk>
2024-01-15 01:21:28 +0000

    remove /boot before updating

--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -2,6 +2,7 @@
 HERE=$(dirname $0)
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 # sudo nix-store --verify --check-contents --repair
+sudo rm -rf /boot/*
 $HERE/update.sh $@ && \
 $HERE/switch.sh $@ && \
 $HERE/cleanup.sh
