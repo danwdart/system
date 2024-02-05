ac1e2fda6e88d5ee4ff1ab304a8fc22dfcd1d318
Dan Dart <git@dandart.co.uk>
2024-02-05 18:13:18 +0000

    update substituters

--- tilde/nixpkgs-config.nix
+++ tilde/nixpkgs-config.nix
@@ -2,18 +2,20 @@
     allowUnfree = true;
     substituters = [
       https://cache.nixos.org
-      https://cache.jolharg.com
+      # https://cache.jolharg.com
       https://dandart.cachix.org
       https://nixcache.reflex-frp.org
+      https://miso-haskell.cachix.org
       https://nixcache.webghc.org
       https://cache.iog.io
       https://static-haskell-nix.cachix.org
     ];
     trusted-public-keys = [
       cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
-      cache.jolharg.com:JSK2oHzlOOULEJXAM1sKG7+WvB3bZkO9DtlyljmjfH4=
+      # cache.jolharg.com:JSK2oHzlOOULEJXAM1sKG7+WvB3bZkO9DtlyljmjfH4=
       dandart.cachix.org-1:k7r2DOpcXOY6Hx+qXMrtYjzCt0XH5tmfJHL/faJ088I=
       ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=
+      miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8=
       hydra.webghc.org-1:knW30Yb8EXYxmUZKEl0Vc6t2BDjAUQ5kfC1BKJ9qEG8=
       hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
       static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU=
