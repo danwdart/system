e77964b8d338fc2914e7652c9f399f998cdc1364
Dan Dart <git@dandart.co.uk>
2024-04-13 00:01:02 +0100

    fix desktop services and remove specialisations for other desktops

--- common/configuration-desktop.nix
+++ common/configuration-desktop.nix
@@ -16,58 +16,58 @@ in
   hardware = import ./hardware.nix {pkgs = pkgs; isDesktop = true;};
   networking = import ./networking.nix { lib = lib; hostName = hostName; isDesktop = true; };
   programs = import ./programs.nix { pkgs = pkgs; isDesktop = true; };
-  services = import ./services.nix { pkgs = pkgs; hostName = hostName; hostDir = hostDir; privateDir = privateDir; isDesktop = true; };
+  services = import ./services.nix { pkgs = pkgs; config = config; hostName = hostName; hostDir = hostDir; privateDir = privateDir; isDesktop = true; };
   systemd = import ./systemd.nix { privateDir = privateDir; isDesktop = true; };
   
   home-manager.users.dwd = import ./users/dwd/home.nix { pkgs = pkgs; isDesktop = true; };
 
-  specialisation.server-mode.configuration = {
-    environment.systemPackages = import ./packages/console/packages.nix pkgs;
-    services.xserver.enable = lib.mkForce false;
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-  };
-
-  specialisation.cinnamon.configuration = {
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    services.xserver.desktopManager.cinnamon.enable = true;
-    services.cinnamon.apps.enable = true;
-  };
-
-  specialisation.gnome.configuration = {
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-  };
-
-  specialisation.surf.configuration = {
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    services.xserver.desktopManager.surf-display.enable = true;
-    services.xserver.desktopManager.surf-display.defaultWwwUri = "https://jolharg.com";
-  };
-
-  specialisation.retroarch.configuration = {
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    services.xserver.desktopManager.retroarch.enable = true;
-  };
-
-  specialisation.pantheon.configuration = {
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    services.xserver.desktopManager.pantheon.enable = true;
-  };
-
-  specialisation.enlightenment.configuration = {
-    services.xserver.displayManager.sddm.enable = lib.mkForce false;
-    services.xserver.displayManager.lightdm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    services.xserver.desktopManager.enlightenment.enable = true;
-  };
+  # specialisation.server-mode.configuration = {
+  #   environment.systemPackages = import ./packages/console/packages.nix pkgs;
+  #   services.xserver.enable = lib.mkForce false;
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  # };
+# 
+  # specialisation.cinnamon.configuration = {
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.displayManager.lightdm.enable = true;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  #   services.xserver.desktopManager.cinnamon.enable = true;
+  #   services.cinnamon.apps.enable = true;
+  # };
+# 
+  # specialisation.gnome.configuration = {
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.displayManager.lightdm.enable = true;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  # };
+# 
+  # specialisation.surf.configuration = {
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.displayManager.lightdm.enable = true;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  #   services.xserver.desktopManager.surf-display.enable = true;
+  #   services.xserver.desktopManager.surf-display.defaultWwwUri = "https://jolharg.com";
+  # };
+# 
+  # specialisation.retroarch.configuration = {
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.displayManager.lightdm.enable = true;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  #   services.xserver.desktopManager.retroarch.enable = true;
+  # };
+# 
+  # specialisation.pantheon.configuration = {
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.displayManager.lightdm.enable = true;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  #   services.xserver.desktopManager.pantheon.enable = true;
+  # };
+# 
+  # specialisation.enlightenment.configuration = {
+  #   services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  #   services.xserver.displayManager.lightdm.enable = true;
+  #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+  #   services.xserver.desktopManager.enlightenment.enable = true;
+  # };
 }
