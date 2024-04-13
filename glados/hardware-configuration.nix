492b31d22b559672e3a863a4bca5aca81ddadca2
Dan Dart <git@dandart.co.uk>
2024-04-13 01:52:34 +0100

    use zram for glados

--- glados/hardware-configuration.nix
+++ glados/hardware-configuration.nix
@@ -30,6 +30,14 @@
 
   swapDevices = [ ];
 
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    # numDevices = 1; # Using ZRAM devices as general purpose ephemeral block devices is no longer supported
+    swapDevices = 1;
+    memoryPercent = 50;
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
