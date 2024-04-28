af8f750b208b59f20c569484e260a6c7514c4156
Dan Dart <git@dandart.co.uk>
2024-04-28 18:10:16 +0100

    update game list

--- common/packages/gui/games.nix
+++ common/packages/gui/games.nix
@@ -7,39 +7,41 @@ let pkgs-x86_64 = import <nixos> {
     };
 in
 with pkgs; [
+    # alienarena # no desktop icon # segfaults
     # armagetronad # no desktop icon # failed with exit code 141
     dolphinEmu
     # extremetuxracer
     # frozen-bubble
     # golly # now broken?
     hhexen
-    ioquake3 # no desktop icon
+    ioquake3
     # lbreakout2
-    liberation-circuit
+    # liberation-circuit # no desktop icon # wat
     # ltris
-    megaglest
+    # megaglest # more like meh-a-glest
     # mgba
     # mupen64plus # no desktop icon
     nethack-qt # no desktop icon
-    # nexuiz # no desktop icon # BIG
-    # openarena # no desktop icon # BIG
+    nexuiz # no desktop icon # BIG
+    openarena # no desktop icon # BIG
     # padman?
-    quakespasm # no desktop icon
-    # redeclipse
-    # sauerbraten # no desktop icon
+    # quakespasm # no desktop icon
+    redeclipse
+    # sauerbraten # elsewhere in cli
     # snes9x-gtk # has to compile now?
     # speed_dreams # no desktop icon # keeps compiling
     # stuntrally # BIG
     # superTux # Bigish
     superTuxKart # BIG
     # torcs # no desktop icon
-    # tremulous - broken
+    # tremulous # - broken
+    # trenchbroom
     # trigger # no desktop icon
     uhexen2
-    # urbanterror # no desktop icon # nBIG
-    # warsow - can't download
-    # xonotic # keeps compiling
-    yquake2 # no desktop icon
+    urbanterror # BIG # weirdX
+    warsow # no desktop icon
+    xonotic
+    yquake2
     # zeroadPackages.zeroad-unwrapped # segfaults
     # zeroadPackages.zeroad-data
     # zsnes
@@ -47,8 +49,8 @@ with pkgs; [
     steam
     steamcmd
     steam-run-native
-    quake3e # no desktop icon
-    vkquake # no desktop icon but needs dir
+    quake3e
+    # vkquake # no desktop icon but needs dir
 ] else [
    # pkgs-x86_64.quake3e # no desktop icon
    # pkgs-x86_64.vkquake # no desktop icon but needs dir
