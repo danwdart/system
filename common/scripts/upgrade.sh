39fc1553ab0d4a625809027c765091ed0af24f3d
Dan Dart <git@dandart.co.uk>
2024-07-08 17:08:44 +0100

    update scripts

--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -3,6 +3,6 @@ HERE=$(dirname $0)
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 # sudo nix-store --verify --check-contents --repair
 # sudo rm -rf /boot/*
-$HERE/update.sh $@ && \
+$HERE/update.sh && \
 $HERE/switch.sh $@ && \
 $HERE/cleanup.sh
