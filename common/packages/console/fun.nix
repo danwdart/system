91550f547de62ef20093709ea090c751bdc962b1
Dan Dart <git@dandart.co.uk>
2025-08-07 18:46:20 +0100

    update fun

--- common/packages/console/fun.nix
+++ common/packages/console/fun.nix
@@ -3,7 +3,9 @@ with pkgs; [
     ddate
     fortune
     genact
+    # haxor-news # ERROR Backend 'setuptools.build_meta:__legacy__' is not available. - python3.13-click-7.1.2.drv' failed with exit code 1
     hollywood
+    lavat
     nms
     screenfetch
 ]
