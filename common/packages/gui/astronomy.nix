fc75fffe4857c8fe736d91c1dcaa873f1becb7f1
Dan Dart <git@dandart.co.uk>
2025-08-07 18:46:47 +0100

    no celestia for now

--- common/packages/gui/astronomy.nix
+++ common/packages/gui/astronomy.nix
@@ -1,7 +1,7 @@
 pkgs:
 with pkgs;
 [
-    celestia
+    # celestia # error: builder for '/nix/store/v5qlj66n9d17yk9hy10binp7fgmvh27j-celestia-1.6.4.drv' failed with exit code 1
     kstars
     # stellarium # big build
 ]
