bdc9e2787fb88e93683a7973ca0c57cee58b136b
Dan Dart <git@dandart.co.uk>
2025-08-07 18:48:21 +0100

    gimp3

--- common/packages/gui/graphics.nix
+++ common/packages/gui/graphics.nix
@@ -9,7 +9,7 @@ in
 with pkgs; [
     # blender # fails to build because of python3.11-openusd
     geogebra
-    gimp
+    gimp3
     inkscape
     krita
 ] ++ (if builtins.currentSystem == "x86_64-linux" then [
