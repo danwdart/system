e845c095126c57a4a347729ebe0f51c05a43a2e9
Dan Dart <git@dandart.co.uk>
2025-08-07 18:49:06 +0100

    perhaps?

--- common/packages/gui/office.nix
+++ common/packages/gui/office.nix
@@ -1,9 +1,14 @@
 pkgs:
 with pkgs; [
     # calibre # fails to build with python mechanize
+    # collabora-online # put in services
     # libreoffice-qt6-fresh # takes ages to compile
     libreoffice-qt6
     # libreoffice-fresh # takes ages to compile
     # notion-app-enhanced
     # appflowy
+    # onlyoffice-desktopeditors # put in services
+    # onlyoffice-documentserver # put in services
+    # projectlibre
+    your_spotify
 ]
