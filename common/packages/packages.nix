3bb98ffa0163b588a1a13a0a01610414b690b02d
Dan Dart <git@dandart.co.uk>
2025-08-13 00:17:18 +0100

    nothings

--- common/packages/packages.nix
+++ common/packages/packages.nix
@@ -1,6 +1,4 @@
 pkgs:
 # Any defaults/must-haves go here
-[]
-# Any categorical apps go here
-    ++ import ./console/packages.nix pkgs
+import ./console/packages.nix pkgs
     ++ import ./gui/packages.nix pkgs
