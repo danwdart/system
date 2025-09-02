1db33596ae4b859c1c84f895f707c4d5b100a751
Dan Dart <git@dandart.co.uk>
2025-09-02 18:00:01 +0100

    hamradio stuff

--- common/boot.nix
+++ common/boot.nix
@@ -44,45 +44,45 @@
 
   loader.systemd-boot.enable = lib.mkForce false;
   # https://nixos.wiki/wiki/Linux_kernel
-  # kernelPatches = [
-  #   {
-  #     name = "Amateur Radio support";
-  #     patch = null;
-  #     # TODO pick a cached version / remove most unneccessaries
-  #     extraConfig = ''
-  #             HAMRADIO y
-# 
-  #             #
-  #             # Packet Radio protocols
-  #             #
-  #             AX25 m
-  #             AX25_DAMA_SLAVE y
-  #             NETROM m
-  #             ROSE m
-# 
-  #             #
-  #             # AX.25 network device drivers
-  #             #
-  #             MKISS m
-  #             6PACK m
-  #             BPQETHER m
-  #             BAYCOM_SER_FDX m
-  #             BAYCOM_SER_HDX m
-  #             YAM m
-  #             # end of AX.25 network device drivers
-  #     '';
-  #   }
-  #   # {
-  #   #   name = "Realtime support";
-  #   #   patch = null;
-  #   #   extraConfig = ''
-  #   #           # PREEMPT_BUILT n
-  #   #           PREEMPT_VOLUNTARY n
-  #   #           PREEMPT_RT y
-  #   #           # PREEMPT_DYNAMIC n
-  #   #   '';
-  #   # }
-  # ];
+  kernelPatches = [
+    {
+      name = "Amateur Radio support";
+      patch = null;
+      # TODO pick a cached version / remove most unneccessaries
+      extraConfig = ''
+              HAMRADIO y
+
+              #
+              # Packet Radio protocols
+              #
+              AX25 m
+              AX25_DAMA_SLAVE y
+              NETROM m
+              ROSE m
+
+              #
+              # AX.25 network device drivers
+              #
+              MKISS m
+              6PACK m
+              BPQETHER m
+              BAYCOM_SER_FDX m
+              BAYCOM_SER_HDX m
+              YAM m
+              # end of AX.25 network device drivers
+      '';
+    }
+    # {
+    #   name = "Realtime support";
+    #   patch = null;
+    #   extraConfig = ''
+    #           # PREEMPT_BUILT n
+    #           PREEMPT_VOLUNTARY n
+    #           PREEMPT_RT y
+    #           # PREEMPT_DYNAMIC n
+    #   '';
+    # }
+  ];
 
   # kernelParams = [
   #   "intel_iommu=on"
