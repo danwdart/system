3c85a94a89cea7d813b5ec5d4733ccea53c960e4
Dan Dart <git@dandart.co.uk>
2025-09-02 17:58:54 +0100

    openshot broke

--- common/packages/gui/video-creation.nix
+++ common/packages/gui/video-creation.nix
@@ -8,7 +8,7 @@ with pkgs; [
     simplescreenrecorder
     obs-studio
     olive-editor
-    openshot-qt
+    # openshot-qt # no qtwebengine
     # pitivi # requires triton-llvm which takes 10 years and 100 GB of RAM to build
     # shotcut # won't build because of jack1
 ]
