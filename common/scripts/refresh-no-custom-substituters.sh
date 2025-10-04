b3119a64d7f966f83a3c72684de1133988b10a7d
Dan Dart <git@dandart.co.uk>
2025-10-04 22:37:17 +0100

    upd ssscripts

--- common/scripts/refresh-no-custom-substituters.sh
+++ common/scripts/refresh-no-custom-substituters.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo nixos-rebuild switch --fast -I nixos-config="$PWD"/configuration.nix --option subtituters https://cache.nixos.org $@
+sudo nixos-rebuild switch --no-reexec -I nixos-config="$PWD"/configuration.nix --option subtituters https://cache.nixos.org $@
