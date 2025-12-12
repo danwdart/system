63b1355380b3d32bcf55c73fdf1685918d7d7354
Dan Dart <git@dandart.co.uk>
2025-12-12 19:13:37 +0000

    upgrade: 25.11

--- common/scripts/upgrade.sh
+++ common/scripts/upgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+sudo nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
 # sudo nix-store --verify --check-contents --repair
 # sudo rm -rf /boot/*
 "$HERE"/update.sh && \
