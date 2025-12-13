e134400982b12fb46581af2c81386b74f0f3ef82
Dan Dart <git@dandart.co.uk>
2025-12-13 19:56:15 +0000

    olive-editor errors

--- common/packages/gui/video-creation.nix
+++ common/packages/gui/video-creation.nix
@@ -7,7 +7,7 @@ with pkgs; [
     lightworks
     simplescreenrecorder
     obs-studio
-    olive-editor
+    # olive-editor # /nix/store/zp6r9bxds1hldvkx1vbrk0d1ady17zhh-qtbase-6.10.1/include/QtCore/qstring.h:364:5: error: no type named 'type' in 'struct std::enable_if<false, QString>'
     # openshot-qt # no qtwebengine
     # pitivi # requires triton-llvm which takes 10 years and 100 GB of RAM to build
     # shotcut # won't build because of jack1
